deab62dc464ee7eb18f711d54e1e96a2c5b3885c
Deyvison Estevam <deyvisonestevam@gmail.com>
2020-02-04 20:10:35 -0300

    criando seeder UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php 
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	$dados = [
+    		'name'=>"Administrador",
+    		'email'=>"admin@admin.com",
+    		'password' => bcrypt("admin"),
+    	];
+
+    	if(User::where('email', '=', $dados['email'])->count()){
+    		$user = User::where('email', '=', $dados['email'])->first();
+    		$user->update($dados);
+    	}else{
+    		User::create($dados);
+    	}
+    }
+}
